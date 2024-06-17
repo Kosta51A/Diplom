73b0e5c998ecc955f0bac4873f810b0a46c69af7
Kosta51A <akosta02@yandex.ru>
2024-06-17 21:58:14 +0300

    new ports

--- src/server/server.js
+++ src/server/server.js
@@ -52,7 +52,7 @@ app.post('/reviews', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
